ceaa4e9af2e19b55c64e3f18ecf200942fb05bdf
SalehinShohan <ps.shohan1@gmail.com>
2023-05-19 12:41:58 +0600

    add a delete function

--- index.js
+++ index.js
@@ -101,6 +101,14 @@ async function run() {
       res.send(result)
     })
 
+
+    app.delete('/deleteToy/:id', async(req, res) => {
+      const id = req.params.id;
+      const querry = {_id: new ObjectId(id)}
+      const result = await toysCollection.deleteOne(querry);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
